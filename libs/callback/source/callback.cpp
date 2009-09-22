d070d240e1b5809b16c92bdca67ad13f040da3e0
Joacim Jacobsson <j.jacobsson@gmail.com>
2009-09-22 16:57:39 +0200

    Got rid of the outer "while" in the VM in favor of a goto. Yes. A goto. really.

--- libs/callback/source/callback.cpp
+++ libs/callback/source/callback.cpp
@@ -23,176 +23,176 @@ namespace callback
         callbackHandler ch   =   info->m_callback;
         DebugHandler    dh   =   info->m_Debug;
 
-        bool exit = false;
-        while( !exit )
-        {
-            const Instruction& inst = i[bh->m_IP];
-            ++bh->m_IP;
-            ++bh->m_IC;
-            switch( inst.m_I )
-            {
-            case INST_CALL_DEBUG_FN:
-                //if( dh )
-                //    dh( info, ph, bh );
-                break;
-            case INST_CALL_CONS_FUN:
-                ch(
-                    bh->m_R[inst.m_A1],
-                    ACT_CONSTRUCT,
-                    (void*)bh->m_R[inst.m_A2],
-                    (void**)bh->m_R[inst.m_A3],
-                    info->m_UserData
-                );
-                bh->m_R[inst.m_A1] = 0;
-                bh->m_R[inst.m_A2] = 0;
-                bh->m_R[inst.m_A3] = 0;
-                break;
-            case INST_CALL_EXEC_FUN:
-                bh->m_RE = ch(
-                    bh->m_R[inst.m_A1],
-                    ACT_EXECUTE,
-                    (void*)bh->m_R[inst.m_A2],
-                    (void**)bh->m_R[inst.m_A3],
-                    info->m_UserData
-                );
-                bh->m_R[inst.m_A1] = 0;
-                bh->m_R[inst.m_A2] = 0;
-                bh->m_R[inst.m_A3] = 0;
-                break;
-            case INST_CALL_DEST_FUN:
-                ch(
-                    bh->m_R[inst.m_A1],
-                    ACT_DESTRUCT,
-                    (void*)bh->m_R[inst.m_A2],
-                    (void**)bh->m_R[inst.m_A3],
-                    info->m_UserData
-                );
-                bh->m_R[inst.m_A1] = 0;
-                bh->m_R[inst.m_A2] = 0;
-                bh->m_R[inst.m_A3] = 0;
-                break;
+start:
+		const Instruction& inst = i[bh->m_IP];
+		++bh->m_IP;
+		++bh->m_IC;
+		switch( inst.m_I )
+		{
+		case INST_CALL_DEBUG_FN:
+			//if( dh )
+			//    dh( info, ph, bh );
+			break;
+		case INST_CALL_CONS_FUN:
+			ch(
+				bh->m_R[inst.m_A1],
+				ACT_CONSTRUCT,
+				(void*)bh->m_R[inst.m_A2],
+				(void**)bh->m_R[inst.m_A3],
+				info->m_UserData
+			);
+			bh->m_R[inst.m_A1] = 0;
+			bh->m_R[inst.m_A2] = 0;
+			bh->m_R[inst.m_A3] = 0;
+			break;
+		case INST_CALL_EXEC_FUN:
+			bh->m_RE = ch(
+				bh->m_R[inst.m_A1],
+				ACT_EXECUTE,
+				(void*)bh->m_R[inst.m_A2],
+				(void**)bh->m_R[inst.m_A3],
+				info->m_UserData
+			);
+			bh->m_R[inst.m_A1] = 0;
+			bh->m_R[inst.m_A2] = 0;
+			bh->m_R[inst.m_A3] = 0;
+			break;
+		case INST_CALL_DEST_FUN:
+			ch(
+				bh->m_R[inst.m_A1],
+				ACT_DESTRUCT,
+				(void*)bh->m_R[inst.m_A2],
+				(void**)bh->m_R[inst.m_A3],
+				info->m_UserData
+			);
+			bh->m_R[inst.m_A1] = 0;
+			bh->m_R[inst.m_A2] = 0;
+			bh->m_R[inst.m_A3] = 0;
+			break;
 
-            case INST_CALL_PRUN_FUN:
-                bh->m_RE = ch(
-                    bh->m_R[inst.m_A1],
-                    ACT_PRUNE,
-                    (void*)bh->m_R[inst.m_A2],
-                    (void**)bh->m_R[inst.m_A3],
-                    info->m_UserData
-                );
-                bh->m_R[inst.m_A1] = 0;
-                bh->m_R[inst.m_A2] = 0;
-                bh->m_R[inst.m_A3] = 0;
-                break;
-            case INST_CALL_MODI_FUN:
-                bh->m_RE = ch(
-                    bh->m_R[inst.m_A1],
-                    ACT_MODIFY,
-                    (void*)bh->m_R[inst.m_A2],
-                    (void**)bh->m_R[inst.m_A3],
-                    info->m_UserData
-                );
-                bh->m_R[inst.m_A1] = 0;
-                bh->m_R[inst.m_A2] = 0;
-                bh->m_R[inst.m_A3] = 0;
-                break;
-            case INST_JABC_R_EQUA_C:
-                if( bh->m_RE == inst.m_A2 )
-                    bh->m_IP = inst.m_A1;
-                break;
-            case INST_JABC_R_DIFF_C:
-                if( bh->m_RE != inst.m_A2 )
-                    bh->m_IP = inst.m_A1;
-                break;
-            case INST_JABC_C_EQUA_B:
-                if( inst.m_A2 == *((int*)&(bss[inst.m_A3])) )
-                    bh->m_IP = inst.m_A1;
-                break;
-            case INST_JABC_C_DIFF_B:
-                if( inst.m_A2 != *((int*)&(bss[inst.m_A3])) )
-                    bh->m_IP = inst.m_A1;
-                break;
-            case INST_JABB_C_EQUA_B:
-                if( inst.m_A2 == *((int*)&(bss[inst.m_A3])) )
-                    bh->m_IP = *((int*)&(bss[inst.m_A1]));
-                break;
-            case INST_JABB_C_DIFF_B:
-                if( inst.m_A2 != *((int*)&(bss[inst.m_A3])) )
-                    bh->m_IP = *((int*)&(bss[inst.m_A1]));
-                break;
-            case INST_JABB_B_EQUA_B:
-                if( *((int*)&(bss[inst.m_A2])) == *((int*)&(bss[inst.m_A3])) )
-                    bh->m_IP = *((int*)&(bss[inst.m_A1]));
-                break;
-            case INST_JABB_B_DIFF_B:
-                if( *((int*)&(bss[inst.m_A2])) != *((int*)&(bss[inst.m_A3])) )
-                    bh->m_IP = *((int*)&(bss[inst.m_A1]));
-                break;
-            case INST_JABC_CONSTANT:
-                bh->m_IP = inst.m_A1;
-                break;
-            case INST_JREC_CONSTANT:
-                bh->m_IP += inst.m_A1;
-                break;
-            case INST_JABB_BSSVALUE:
-                bh->m_IP = *((int*)&(bss[inst.m_A1]));
-                break;
-            case INST_JREB_BSSVALUE:
-                bh->m_IP += *((int*)&(bss[inst.m_A1]));
-                break;
-            case INST_JABC_S_C_IN_B:
-                bh->m_IP = inst.m_A1;
-                *((int*)&(bss[inst.m_A2])) = inst.m_A3;
-                break;
-            case INST_JREC_S_C_IN_B:
-                bh->m_IP += inst.m_A1;
-                *((int*)&(bss[inst.m_A2])) = inst.m_A3;
-                break;
-            case INST_JABB_S_C_IN_B:
-                bh->m_IP = *((int*)&(bss[inst.m_A1]));
-                *((int*)&(bss[inst.m_A2])) = inst.m_A3;
-                break;
-            case INST_JREB_S_C_IN_B:
-                bh->m_IP += *((int*)&(bss[inst.m_A1]));
-                *((int*)&(bss[inst.m_A2])) = inst.m_A3;
-                break;
-            case INST__STORE_R_IN_B:
-                *((int*)&(bss[inst.m_A1])) = bh->m_RE;
-                break;
-            case INST__STORE_B_IN_R:
-                bh->m_RE = *((int*)&(bss[inst.m_A1]));
-                break;
-            case INST__STORE_C_IN_B:
-                *((int*)&(bss[inst.m_A1])) = inst.m_A2;
-                break;
-            case INST__STORE_B_IN_B:
-                *((int*)&(bss[inst.m_A1])) = *((int*)&(bss[inst.m_A2]));
-                break;
-            case INST__STORE_C_IN_R:
-                bh->m_RE = inst.m_A1;
-                break;
-            case INST_STORE_PD_IN_B:
-                *(int*)(&bss[inst.m_A1]) = (int)(&data[inst.m_A2]);
-                break;
-            case INST_STORE_PB_IN_R:
-                bh->m_R[inst.m_A1] = (int)(&bss[inst.m_A2]);
-                break;
-            case INST__INC_BSSVALUE:
-                *((int*)&(bss[inst.m_A1])) += inst.m_A2;
-                break;
-            case INST__DEC_BSSVALUE:
-                *((int*)&(bss[inst.m_A1])) += inst.m_A2;
-                break;
-            case INST_LOAD_REGISTRY:
-                bh->m_R[inst.m_A1] = (((int)inst.m_A2)<<16) + inst.m_A3;
-                break;
-            case INST_______SUSPEND:
-                bh->m_IP = 0;
-                exit     = true;
-                break;
-            }
-        }
-        return bh->m_RE;
+		case INST_CALL_PRUN_FUN:
+			bh->m_RE = ch(
+				bh->m_R[inst.m_A1],
+				ACT_PRUNE,
+				(void*)bh->m_R[inst.m_A2],
+				(void**)bh->m_R[inst.m_A3],
+				info->m_UserData
+			);
+			bh->m_R[inst.m_A1] = 0;
+			bh->m_R[inst.m_A2] = 0;
+			bh->m_R[inst.m_A3] = 0;
+			break;
+		case INST_CALL_MODI_FUN:
+			bh->m_RE = ch(
+				bh->m_R[inst.m_A1],
+				ACT_MODIFY,
+				(void*)bh->m_R[inst.m_A2],
+				(void**)bh->m_R[inst.m_A3],
+				info->m_UserData
+			);
+			bh->m_R[inst.m_A1] = 0;
+			bh->m_R[inst.m_A2] = 0;
+			bh->m_R[inst.m_A3] = 0;
+			break;
+		case INST_JABC_R_EQUA_C:
+			if( bh->m_RE == inst.m_A2 )
+				bh->m_IP = inst.m_A1;
+			break;
+		case INST_JABC_R_DIFF_C:
+			if( bh->m_RE != inst.m_A2 )
+				bh->m_IP = inst.m_A1;
+			break;
+		case INST_JABC_C_EQUA_B:
+			if( inst.m_A2 == *((int*)&(bss[inst.m_A3])) )
+				bh->m_IP = inst.m_A1;
+			break;
+		case INST_JABC_C_DIFF_B:
+			if( inst.m_A2 != *((int*)&(bss[inst.m_A3])) )
+				bh->m_IP = inst.m_A1;
+			break;
+		case INST_JABB_C_EQUA_B:
+			if( inst.m_A2 == *((int*)&(bss[inst.m_A3])) )
+				bh->m_IP = *((int*)&(bss[inst.m_A1]));
+			break;
+		case INST_JABB_C_DIFF_B:
+			if( inst.m_A2 != *((int*)&(bss[inst.m_A3])) )
+				bh->m_IP = *((int*)&(bss[inst.m_A1]));
+			break;
+		case INST_JABB_B_EQUA_B:
+			if( *((int*)&(bss[inst.m_A2])) == *((int*)&(bss[inst.m_A3])) )
+				bh->m_IP = *((int*)&(bss[inst.m_A1]));
+			break;
+		case INST_JABB_B_DIFF_B:
+			if( *((int*)&(bss[inst.m_A2])) != *((int*)&(bss[inst.m_A3])) )
+				bh->m_IP = *((int*)&(bss[inst.m_A1]));
+			break;
+		case INST_JABC_CONSTANT:
+			bh->m_IP = inst.m_A1;
+			break;
+		case INST_JREC_CONSTANT:
+			bh->m_IP += inst.m_A1;
+			break;
+		case INST_JABB_BSSVALUE:
+			bh->m_IP = *((int*)&(bss[inst.m_A1]));
+			break;
+		case INST_JREB_BSSVALUE:
+			bh->m_IP += *((int*)&(bss[inst.m_A1]));
+			break;
+		case INST_JABC_S_C_IN_B:
+			bh->m_IP = inst.m_A1;
+			*((int*)&(bss[inst.m_A2])) = inst.m_A3;
+			break;
+		case INST_JREC_S_C_IN_B:
+			bh->m_IP += inst.m_A1;
+			*((int*)&(bss[inst.m_A2])) = inst.m_A3;
+			break;
+		case INST_JABB_S_C_IN_B:
+			bh->m_IP = *((int*)&(bss[inst.m_A1]));
+			*((int*)&(bss[inst.m_A2])) = inst.m_A3;
+			break;
+		case INST_JREB_S_C_IN_B:
+			bh->m_IP += *((int*)&(bss[inst.m_A1]));
+			*((int*)&(bss[inst.m_A2])) = inst.m_A3;
+			break;
+		case INST__STORE_R_IN_B:
+			*((int*)&(bss[inst.m_A1])) = bh->m_RE;
+			break;
+		case INST__STORE_B_IN_R:
+			bh->m_RE = *((int*)&(bss[inst.m_A1]));
+			break;
+		case INST__STORE_C_IN_B:
+			*((int*)&(bss[inst.m_A1])) = inst.m_A2;
+			break;
+		case INST__STORE_B_IN_B:
+			*((int*)&(bss[inst.m_A1])) = *((int*)&(bss[inst.m_A2]));
+			break;
+		case INST__STORE_C_IN_R:
+			bh->m_RE = inst.m_A1;
+			break;
+		case INST_STORE_PD_IN_B:
+			*(int*)(&bss[inst.m_A1]) = (int)(&data[inst.m_A2]);
+			break;
+		case INST_STORE_PB_IN_R:
+			bh->m_R[inst.m_A1] = (int)(&bss[inst.m_A2]);
+			break;
+		case INST__INC_BSSVALUE:
+			*((int*)&(bss[inst.m_A1])) += inst.m_A2;
+			break;
+		case INST__DEC_BSSVALUE:
+			*((int*)&(bss[inst.m_A1])) += inst.m_A2;
+			break;
+		case INST_LOAD_REGISTRY:
+			bh->m_R[inst.m_A1] = (((int)inst.m_A2)<<16) + inst.m_A3;
+			break;
+		case INST_______SUSPEND:
+			bh->m_IP = 0;
+			goto exit;
+			break;
+		}
+		goto start;
+exit:
+
+		return bh->m_RE;
     }
 
 }
