264feb3f21217746740d5d8365f02a21d8cafaea
Joacim Jacobsson <j.jacobsson@gmail.com>
2009-10-07 10:07:47 +0200

    Just playing with the Qt exmaple stuff from the book...

--- apps/cts/source/main.cpp
+++ apps/cts/source/main.cpp
@@ -1,10 +1,31 @@
 #include <QtGui/QApplication>
-#include <QtGui/QLabel>
+#include <QtGui/QHBoxLayout>
+#include <QtGui/QSlider>
+#include <QtGui/QSpinBox>
+#include <QtGui/QPushButton>
 
-int main( int argc, char* argv[] )
+int main(int argc, char *argv[])
 {
-	QApplication app( argc, argv );
-	QLabel* label = new QLabel( "Hello QT!" );
-	label->show();
-	return app.exec();
+    QApplication app(argc, argv);
+    QWidget *window = new QWidget;
+    window->setWindowTitle("<h1>Enter Your Age</h1>");
+    QSpinBox *spinBox = new QSpinBox;
+    QSlider *slider = new QSlider(Qt::Horizontal);
+    QPushButton* button = new QPushButton("Quit");
+
+    spinBox->setRange(0, 130);
+    slider->setRange(0, 130);
+    QObject::connect(spinBox, SIGNAL(valueChanged(int)),
+                     slider, SLOT(setValue(int)));
+    QObject::connect(slider, SIGNAL(valueChanged(int)),
+                     spinBox, SLOT(setValue(int)));
+    QObject::connect( button, SIGNAL(clicked()), &app, SLOT(quit()) );
+    spinBox->setValue(35);
+    QHBoxLayout *layout = new QHBoxLayout;
+    layout->addWidget(spinBox);
+    layout->addWidget(slider);
+    layout->addWidget(button);
+    window->setLayout(layout);
+    window->show();
+    return app.exec();
 }
