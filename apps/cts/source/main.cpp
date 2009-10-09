0eee8aa0b8899b9f713f16aa6a30d7c91b9b9cf0
Joacim Jacobsson <j.jacobsson@gmail.com>
2009-10-09 16:14:50 +0200

    And now cts launches the main window built in the designer...

--- apps/cts/source/main.cpp
+++ apps/cts/source/main.cpp
@@ -1,44 +1,16 @@
 #include <QtGui/QApplication>
-#include <QtGui/QHBoxLayout>
-#include <QtGui/QSlider>
-#include <QtGui/QSpinBox>
-#include <QtGui/QPushButton>
-#include <QtGui/QFileDialog>
+#include <QtGui/QMainWindow>
 
-#include "FindDialog.h"
+#include "../ui/ui_main_window.h"
 
 int main(int argc, char *argv[])
 {
     QApplication app(argc, argv);
-    QWidget *window = new QWidget;
-    window->setWindowTitle("Enter Your Age");
-    QSpinBox *spinBox = new QSpinBox;
-    QSlider *slider = new QSlider(Qt::Horizontal);
-    QPushButton* button = new QPushButton("Quit");
 
-    spinBox->setRange(0, 130);
-    slider->setRange(0, 130);
-    QObject::connect(spinBox, SIGNAL(valueChanged(int)),
-                     slider, SLOT(setValue(int)));
-    QObject::connect(slider, SIGNAL(valueChanged(int)),
-                     spinBox, SLOT(setValue(int)));
-    QObject::connect( button, SIGNAL(clicked()), &app, SLOT(quit()) );
-    spinBox->setValue(35);
-    QHBoxLayout *layout = new QHBoxLayout;
-    layout->addWidget(spinBox);
-    layout->addWidget(slider);
-    layout->addWidget(button);
-    window->setLayout(layout);
+	Ui::MainWindow ui;
+	QMainWindow* window = new QMainWindow;
+	ui.setupUi( window );
     window->show();
 
-    FindDialog* dialog = new FindDialog;
-    dialog->show();
-
-    QFileDialog::getOpenFileName(0x0,
-         "Open Image", "", "Behaviour Tree Sources (*.bts);;Behaviour Tree Headers (*.bth)" );
-/*
-	QFileDialog* fd = new QFileDialog;
-	fd->show();
-*/
     return app.exec();
 }
