c741ba5c3112f3cbf8f40b28b3ee0b40701f9142
Joacim Jacobsson <j.jacobsson@gmail.com>
2010-04-07 07:40:37 +0200

    I'm going to stop changing file-names now. I promise.

--- libs/cb_gen/include/cb_gen/cb_gen.h
+++ libs/cb_gen/include/cb_gen/cb_gen.h
@@ -12,7 +12,56 @@
 #ifndef CB_GEN_H_INCLUDED
 #define CB_GEN_H_INCLUDED
 
-#include "function.h"
-#include "program.h"
+#include <vector>
+#include <btree/btree.h>
+#include <callback/callback.h>
+
+namespace cb_gen {
+
+typedef unsigned int uint;
+
+struct Function;
+struct Program;
+
+struct FunctionEntry
+{
+  Function* m_Function;
+  hash_t    m_Id;
+};
+
+struct JumpTarget
+{
+  Function*    m_Function;
+  unsigned int m_Offset;
+};
+
+typedef std::vector<Instruction> InstList;
+typedef std::vector<FunctionEntry> FunctionList;
+typedef std::vector<JumpTarget> JumpTargets;
+
+struct Function
+{
+  Program* m_P;
+  InstList m_I;
+  uint     m_Memory;
+};
+
+struct Program
+{
+  FunctionList  m_F;
+  InstList      m_I;
+  JumpTargets   m_J;
+  uint          m_Memory;
+};
+
+void init( Program* );
+void init( Function* );
+
+void destroy( Program* );
+void destroy( Function* );
+
+void generate( BehaviorTreeContext, Program* );
+
+}
 
 #endif /* CB_GEN_H_INCLUDED */
